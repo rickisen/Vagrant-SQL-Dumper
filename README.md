c6446acc3d2b61537fee4e108b9cc8a7a0dc0388
rickisen <rickard.spam@gmail.com>
2016-02-20 16:25:16 +0100

    Added help section and a better description in README.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Vagrant-SQL-Dumper
-Script that logs on to a vagrant vm and dumps a sql database to a output file, it's ment to be run from within a subfolder to a vagrant-file
+Script that logs on to a vagrant vm and dumps a sql database. 
+it is ment to be run from withn a folder inside a vagrant structure.
+
+#Usage
+./vagrantSqlDump.sh [-h host][-u username][-d database][-o outputfile][-k keysdirectory]
+
+
+#Defaults
+host     = 192.168.33.11
+
+username = vagrant 
+Used to connect to the ssh server running on the vm, not the mysql server
+
+database = database 
+
+outputFile = "$( realpath $( dirname $0 ) )/sqlDump"-"$( date "+%y%m%d-%H%M%S" )".sql 
+Should end up looking something like: /home/user/projects/project/folder/sqlDump-160220-133700.sql
+if the vagrantSqlDump.sh script is located in /home/user/projects/project/folder/
+
+keysdirectory
+This is a subfolder of the hidden folder ".vagrant" that holds the "private_key" file. 
+This file is used to connect to the vagrant vm via ssh. 
+If not specified by the user, the vagrantSqlDump script will automatically find this folder by looking in parrent 
+folders from where the vagrantSqlDump script is located.
--- vagrantSqlDump.sh
+++ vagrantSqlDump.sh
@@ -9,6 +9,35 @@ username=vagrant # to connect to the ssh server running on the vm, not the mysql
 database=database 
 outputFile="$( realpath $( dirname $0 ) )/sqlDump"-"$( date "+%y%m%d-%H%M%S" )".sql # /home/user/projects/project/folder/database-160220-133700.sql
 
+if [[ $1 == "--help" ]]; then
+	echo '
+		Script that logs on to a vagrant vm and dumps a sql database. 
+		it is ment to be run from withn a folder inside a vagrant structure.
+
+		Usage: ./vagrantSqlDump.sh [-h host][-u username][-d database][-o outputfile][-k keysdirectory]
+
+
+		Defaults:
+
+		host     = 192.168.33.11
+
+		username = vagrant 
+		Used to connect to the ssh server running on the vm, not the mysql server
+
+		database = database 
+
+		outputFile = "$( realpath $( dirname $0 ) )/sqlDump"-"$( date "+%y%m%d-%H%M%S" )".sql 
+		Should end up looking something like: /home/user/projects/project/folder/sqlDump-160220-133700.sql
+		if the vagrantSqlDump.sh script is located in /home/user/projects/project/folder/
+
+		keysdirectory
+		This is a subfolder of the hidden folder ".vagrant" that holds the "private_key" file. 
+		This file is used to connect to the vagrant vm via ssh. 
+		If not specified by the user, the vagrantSqlDump script will automatically find this folder by looking in parrent 
+		folders from where the vagrantSqlDump script is located.
+	'
+	exit 
+fi
 # parse the input-parameters with getops and overide the defaults
 OPTIND=1 # Reset in case getopts has been used previously in the shell.
 while getopts "h:u:d:o:k:" opt; do
@@ -44,3 +73,4 @@ fi
 echo "Trying to run mysqldump on $hostname as $username (it wont actually add it to the list of know hosts, it's all lies)" 
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i private_key "$username"@"$hostname" \
 	"mysqldump -h localhost -u root --password='root' $database " > $outputFile
+
